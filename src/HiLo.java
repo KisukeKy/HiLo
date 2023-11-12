310c118b385df2a153c4372d2eb7aab23a3da334
KYLE_ROGERS <rogerskyleart@gmail.com>
2023-11-12 17:23:32 -0500

    update hilo

--- src/HiLo.java
+++ src/HiLo.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class HiLo {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        String playAgain = "";
+        do{
         //Create a random number for the user to guess
 
         int theNumber = (int)(Math.random() * 100);
         //System.out.println(theNumber);
-        int guess = 96;
+        int guess = 0;
 
         while (guess != theNumber) {
         System.out.println("Guess a number between 1 and 100: ");
@@ -17,10 +19,11 @@ public class HiLo {
             System.out.println(guess + " is too low. Try again!");
             else if (guess > theNumber) 
                 System.out.println(guess + " is too high. Try again!");
-            else
+            else 
                 System.out.println(guess + " is correct. You win!");
-    // end of while loop for guessing. 
-        }
+        }// end of while loop for guessing.
+        System.out.println("Would you like to play again (y/n)?"); 
+        }while (playAgain.equalsIgnoreCase("y"));
     }
 }
 
