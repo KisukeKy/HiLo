93611fb8655c67376c89d5175845567285f6a6a3
KYLE_ROGERS <rogerskyleart@gmail.com>
2023-11-16 22:02:02 -0500

    again

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-
-
 public class HiLo {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
@@ -11,7 +9,6 @@ public class HiLo {
         int theNumber = (int)(Math.random() * 100);
         //System.out.println(theNumber);
         int guess = 0;
-
         while (guess != theNumber) {
         System.out.println("Guess a number between 1 and 100: ");
         guess = scanner.nextInt();
@@ -23,6 +20,7 @@ public class HiLo {
                 System.out.println(guess + " is correct. You win!");
         }// end of while loop for guessing.
         System.out.println("Would you like to play again (y/n)?"); 
+        playAgain = scanner.next();
         }while (playAgain.equalsIgnoreCase("y"));
     }
 }
