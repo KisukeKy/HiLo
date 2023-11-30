02fb9350dd125e272db1afa303970e905f98e34c
KYLE_ROGERS <rogerskyleart@gmail.com>
2023-11-30 09:21:06 -0500

    c

--- src/HiLo.java
+++ src/HiLo.java
@@ -19,7 +19,7 @@ public class HiLo {
             else 
                 System.out.println(guess + " is correct. You win!");
         }// end of while loop for guessing.
-        //add more
+        //
         System.out.println("Would you like to play again (y/n)?"); 
         playAgain = scanner.next();
         }while (playAgain.equalsIgnoreCase("y"));
